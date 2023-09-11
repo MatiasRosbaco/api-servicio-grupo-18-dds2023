968a99015e0ab443ba159c0bb6bcc5527b8a3efe
MatiasRosbaco <mati.rosbaco@gmail.com>
2023-09-11 13:55:56 -0300

    init proyecto

---
+++ servicio/src/test/java/Grupo8/servicio/ServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo8.servicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
